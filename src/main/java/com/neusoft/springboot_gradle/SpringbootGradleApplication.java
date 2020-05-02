f6b9d269277af7d6c428745eb339299a973f96ad
NienKisan <wenbin2008ok@163.com>
2020-05-02 12:02:25 +0800

    gradle空框架

---
+++ src/main/java/com/neusoft/springboot_gradle/SpringbootGradleApplication.java
@@ -0,0 +1,13 @@
+package com.neusoft.springboot_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neusoft/springboot_gradle/SpringbootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neusoft.springboot_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
